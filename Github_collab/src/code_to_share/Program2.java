5b22a11e30b142b9064e24e2fc7dfb7947aca770
imgma <imgma@LAPTOP-5G17KOSG>
2023-06-21 16:34:39 -0400

    Program2 pushed

---
+++ Github_collab/src/code_to_share/Program2.java
@@ -0,0 +1,10 @@
+package code_to_share;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is Program2");
+	}
+
+}
